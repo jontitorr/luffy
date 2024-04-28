a0a932c79f71a6cb90a2b4d741a28ac1336a862c
jontitorr <mail@jontitor.com>
2024-04-28 12:22:27 -0500

    feat: :sparkles: show MyAnimeList avatar URL and username in settings

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,8 @@
+import "package:cached_network_image/cached_network_image.dart";
 import "package:flutter/material.dart";
 import "package:flutter/services.dart";
+import "package:luffy/api/mal.dart";
 import "package:luffy/screens/browse.dart";
-import "package:luffy/screens/history.dart";
 import "package:luffy/screens/list.dart";
 import "package:luffy/screens/search_sources.dart";
 import "package:luffy/screens/settings.dart";
@@ -17,14 +18,7 @@ class _HomeScreenState extends State<HomeScreen>
     with SingleTickerProviderStateMixin {
   int _currentIndex = 0;
   late TabController _tabController;
-
-  final List<Widget> _tabs = [
-    const ListScreen(),
-    const HistoryScreen(),
-    const BrowseScreen(),
-    const SearchScreenSources(),
-    const SettingsScreen(),
-  ];
+  late Future<UserInfo?> _userInfoFuture;
 
   void _handleTabChange() {
     if (!_tabController.indexIsChanging) {
@@ -39,11 +33,12 @@ class _HomeScreenState extends State<HomeScreen>
     super.initState();
 
     _tabController = TabController(
-      length: _tabs.length,
+      length: 4,
       vsync: this,
     );
 
     _tabController.addListener(_handleTabChange);
+    _userInfoFuture = MalService.getUserInfo();
 
     if (WidgetsBinding
             .instance.platformDispatcher.views.first.physicalSize.width <
@@ -71,56 +66,71 @@ class _HomeScreenState extends State<HomeScreen>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: TabBarView(
-        controller: _tabController,
-        physics:
-            const NeverScrollableScrollPhysics(), // disable swiping between tabs
-        children: _tabs,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        currentIndex: _currentIndex,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        selectedFontSize: 10,
-        unselectedFontSize: 10,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
+    return FutureBuilder(
+      future: _userInfoFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
 
-          _tabController.animateTo(index);
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bookmark_border),
-            label: "My Lists",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: "History",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.grid_view),
-            label: "Browse",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.tv),
-            label: "Search All",
-          ),
-          BottomNavigationBarItem(
-            icon: CircleAvatar(
-              backgroundImage: NetworkImage(
-                "https://avatars.githubusercontent.com/u/59069386?v=4",
+        final userInfo = snapshot.data;
+
+        return Scaffold(
+          body: TabBarView(
+            controller: _tabController,
+            physics: const NeverScrollableScrollPhysics(),
+            children: [
+              const ListScreen(),
+              const BrowseScreen(),
+              const SearchScreenSources(),
+              SettingsScreen(
+                userInfo: userInfo,
               ),
-              radius: 12,
-            ),
-            label: "Account",
+            ],
           ),
-        ],
-      ),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Theme.of(context).colorScheme.surface,
+            currentIndex: _currentIndex,
+            showSelectedLabels: true,
+            showUnselectedLabels: true,
+            selectedFontSize: 10,
+            unselectedFontSize: 10,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+
+              _tabController.animateTo(index);
+            },
+            items: [
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.bookmark_border),
+                label: "My Lists",
+              ),
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.grid_view),
+                label: "Browse",
+              ),
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: "Search All",
+              ),
+              BottomNavigationBarItem(
+                icon: CircleAvatar(
+                  backgroundImage: CachedNetworkImageProvider(
+                    userInfo?.picture ?? "",
+                  ),
+                  radius: 12,
+                ),
+                label: "Account",
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,12 +1,16 @@
 import "package:flutter/material.dart";
+import "package:luffy/api/mal.dart";
 import "package:luffy/main.dart";
 
 class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({super.key});
+  const SettingsScreen({super.key, required this.userInfo});
+
+  final UserInfo? userInfo;
 
   @override
   Widget build(BuildContext context) {
     final malToken = MyApp.of(context)!.malToken;
+    final info = userInfo;
 
     return Scaffold(
       appBar: AppBar(
@@ -17,21 +21,24 @@ class SettingsScreen extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          if (malToken != null) ...[
-            const Padding(
-              padding: EdgeInsets.all(16.0),
+          if (info != null) ...[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
               child: Row(
                 children: [
                   CircleAvatar(
                     backgroundImage: NetworkImage(
-                      "https://avatars.githubusercontent.com/u/59069386?v=4",
+                      info.picture ?? "",
                     ),
                     radius: 36,
                   ),
-                  SizedBox(width: 16),
+                  const SizedBox(width: 16),
                   Text(
-                    "Username",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    info.name,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ],
               ),
